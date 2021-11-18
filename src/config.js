beb74f515ada35a0beab6b1e7bb08cadf1896966
Alina <imlvovaalina@gmail.com>
2021-11-18 14:35:49 +0400

    HM-91: Display config.

--- src/config.js
+++ src/config.js
@@ -1,15 +1,16 @@
 export let config = function(){
-    switch(env){
-        case 'development':
-            return {api: 'http://localhost:10000'};
+    console.log(env);
+    // switch(env){
+    //     case 'development':
+    //         return {api: 'http://localhost:10000'};
 
-        case 'production':
-            return {api: 'https://houme-api.herokuapp.com'};
+    //     case 'production':
+    //         return {api: 'https://houme-api.herokuapp.com'};
         
-        case 'qa':
-            return {api: 'https://houmly-dev-api.herokuapp.com'}
+    //     case 'qa':
+    //         return {api: 'https://houmly-dev-api.herokuapp.com'}
 
-        default:
-            return {api: 'http://localhost:10000'};
-    }
+    //     default:
+    //         return {api: 'http://localhost:10000'};
+    // }
 };

5b57f67ad34e8485584c3714b014a0dc3841b4b5
Alina <imlvovaalina@gmail.com>
2021-11-25 18:21:14 +0400

    HM-119: Version change.

--- src/app.svelte
+++ src/app.svelte
@@ -42,7 +42,7 @@
 	</Notifications>
 
 	{#if (envmt != "production") } 
-	<div class="version-container">version: 0.2.16</div>
+	<div class="version-container">version: 0.2.17</div>
 	{/if}
 </main>
 

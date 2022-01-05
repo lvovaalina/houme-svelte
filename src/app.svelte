0b21830b7a7899e834ab3666185a7c615c62e5f7
Alina <imlvovaalina@gmail.com>
2022-01-05 13:01:59 +0100

    Version change.

--- src/app.svelte
+++ src/app.svelte
@@ -54,7 +54,7 @@
 	</Notifications>
 
 	{#if (envmt != "production") } 
-	<div class="version-container">version: 0.4.02</div>
+	<div class="version-container">version: 0.4.03</div>
 	{/if}
 </main>
 

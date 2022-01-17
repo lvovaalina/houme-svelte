4db074eb3bab0b927e1df266141c98ef397ced73
Alina <imlvovaalina@gmail.com>
2022-01-17 02:16:45 +0100

    Https redirect.

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const path = require('path');
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 const app = express();
 app.use(express.static('public'));
+
+app.enable('trust proxy')
+app.use((req, res, next) => {
+    req.secure ? next() : res.redirect('https://' + req.headers.host + req.url);
+    console.log("SECURE", req.secure);
+})
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
